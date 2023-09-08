ee10f3b96d0c62169d3a62fbf151da6a0d84c87b
khzanataza <sorteonft@gmail.com>
2023-09-08 18:59:29 +0200

    intento

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,39 @@
 import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Index from "./Index"; // Importa tu componente Index.js
-import AboutUs from "./AboutUs"; // Importa tu componente AboutUs.js
+import Link from "next/link"; // Importa Link de Next.js
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Router>
-      <Fragment>
-        <DefaultSeo
-          title="Youtube Thumbnail Downloader"
-          description="Download high-quality thumbnails from YouTube videos."
-          canonical="https://www.youtubethumbnaildownload4k.com/"
-          openGraph={{
-            url: "https://www.youtubethumbnaildownload4k.com/",
-            title: "Youtube Thumbnail Downloader",
-            description: "Download high-quality thumbnails from YouTube videos.",
-            site_name: "Youtube Thumbnail Downloader",
-          }}
-        />
-        <Switch>
-          <Route path="/" exact component={Index} />
-          <Route path="/about" component={AboutUs} />
-          {/* Agrega más rutas si es necesario */}
-        </Switch>
-      </Fragment>
-    </Router>
+    <Fragment>
+      <DefaultSeo
+        title="Youtube Thumbnail Downloader"
+        description="Download high-quality thumbnails from YouTube videos."
+        canonical="https://www.youtubethumbnaildownload4k.com/"
+        openGraph={{
+          url: "https://www.youtubethumbnaildownload4k.com/",
+          title: "Youtube Thumbnail Downloader",
+          description: "Download high-quality thumbnails from YouTube videos.",
+          site_name: "Youtube Thumbnail Downloader",
+        }}
+      />
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          {/* Agrega más enlaces si es necesario */}
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </Fragment>
   );
 }
 
