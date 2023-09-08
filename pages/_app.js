5e999a015105616815d008db1a10d85f42443de5
khzanataza <sorteonft@gmail.com>
2023-09-08 18:54:28 +0200

    aboutus

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,32 @@
-import "../styles/index.css";
 import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Index from "./Index"; // Importa tu componente Index.js
+import AboutUs from "./AboutUs"; // Importa tu componente AboutUs.js
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Fragment>
-      <DefaultSeo
-        title="Youtube Thumbnail Downloader"
-        description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://www.youtubethumbnaildownload4k.com/"
-        openGraph={{
-          url: "https://www.youtubethumbnaildownload4k.com/",
-          title: "Youtube Thumbnail Downloader",
-          description: "Download high-quality thumbnails from YouTube videos.",
-          site_name: "Youtube Thumbnail Downloader",
-        }}
-      />
-      <Component {...pageProps} />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <DefaultSeo
+          title="Youtube Thumbnail Downloader"
+          description="Download high-quality thumbnails from YouTube videos."
+          canonical="https://www.youtubethumbnaildownload4k.com/"
+          openGraph={{
+            url: "https://www.youtubethumbnaildownload4k.com/",
+            title: "Youtube Thumbnail Downloader",
+            description: "Download high-quality thumbnails from YouTube videos.",
+            site_name: "Youtube Thumbnail Downloader",
+          }}
+        />
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/about" component={AboutUs} />
+          {/* Agrega más rutas si es necesario */}
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
