a3971cddac1212417a42a5310629d547b8096594
khzanataza <sorteonft@gmail.com>
2023-09-21 22:09:51 +0200

    ads

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,59 @@ import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
 import Head from 'next/head';
 
+
+const LeftBanner = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Cargar el script de anuncios de manera asincrónica
+      const script = document.createElement('script');
+      script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9616854314725667';
+      script.async = true;
+      document.body.appendChild(script);
+
+      // Ejecutar el script cuando se cargue
+      script.onload = () => {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      };
+    }
+  }, []);
+
+  return (
+    <div className="left-banner" style={{ position: 'fixed', left: 0, top: '50%', transform: 'translateY(-50%)', zIndex: 999 }}>
+      <ins className="adsbygoogle"
+           style={{ display: 'inline-block', width: '160px', height: '600px' }}
+           data-ad-client="ca-pub-9616854314725667"
+           data-ad-slot="4757685506"></ins>
+    </div>
+  );
+};
+
+const RightBanner = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Cargar el script de anuncios de manera asincrónica
+      const script = document.createElement('script');
+      script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9616854314725667';
+      script.async = true;
+      document.body.appendChild(script);
+
+      // Ejecutar el script cuando se cargue
+      script.onload = () => {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      };
+    }
+  }, []);
+
+  return (
+    <div className="right-banner" style={{ position: 'fixed', right: 0, top: '50%', transform: 'translateY(-50%)', zIndex: 999 }}>
+      <ins className="adsbygoogle"
+           style={{ display: 'inline-block', width: '160px', height: '600px' }}
+           data-ad-client="ca-pub-9616854314725667"
+           data-ad-slot="4757685506"></ins>
+    </div>
+  );
+};
+
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
--- pages/_document.js
+++ pages/_document.js
@@ -32,14 +32,6 @@ class MyDocument extends Document {
           <Main />
           {/* Contenido optimizado */}
           <div>
-            <h1>YouTube Thumbnail Download Service: Get High-Quality YT Video Thumbnails</h1>
-            <p>Looking for a reliable YouTube Thumbnail Downloader? Look no further! Our platform provides you with the ability to effortlessly save thumbnail images from any YouTube video in multiple resolutions: Full HD (1080p), HD (720p), SD, and even a compact size. Our supported formats encompass a wide range, including YouTube videos in HD, HQ, 1080p, and 4K.</p>
-            <h2>Unlocking the Potential of Our YouTube Thumbnail Grabber</h2>
-            <p>Discover the utility of our YouTube Thumbnail Grabber website. This powerful tool empowers you to obtain captivating thumbnails from any YouTube video. These thumbnails can enhance presentations, enrich animation projects, and fuel a multitude of creative undertakings.</p>
-            {/* ... Otros apartados del texto optimizado ... */}
-            <h2>SEO Implications of Reusing YouTube Thumbnails</h2>
-            <p>In terms of SEO, reusing YouTube thumbnails is not advisable due to Google's indexing practices. Google can distinguish between unique and duplicate thumbnails. However, you can enhance the uniqueness of a thumbnail by applying distinct effects using software like Photoshop. This can rejuvenate the thumbnail's SEO viability.</p>
-            <p><strong>Experience the power of YouTube Thumbnail Downloading today!</strong></p>
             <p>Visit <a href="www.youtubethumbnaildownload4k.com">www.youtubethumbnaildownload4k.com</a> - Copyright 2023</p>
           </div>
           {/* Fin del contenido optimizado */}
